9940ff9cc1c6031de51998caf852ed22fe55f472
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-16 14:58:19 -0700

    W02D3 - pair programming - Word Search assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+  if (letters.length === 0) {
+      return false;
+  }
+  if (word === "") {
+      return false;
+  }
+  const verticalJoin = [];
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (let l = 0; l < horizontalJoin[0].length; l++) {
+      verticalJoin.push([]);
+      if (horizontalJoin[l].includes(word)) return true
+      for (let i = 0; i < horizontalJoin.length; i++) {
+          verticalJoin[l].push(horizontalJoin[i][l])
+          if (verticalJoin[l].join('').includes(word)) return true
+      }
+  }
+  return false
+};
 
 module.exports = wordSearch
